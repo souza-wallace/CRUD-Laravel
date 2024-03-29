92bb15d50fff12cafe23da0ca3de40f776f7aa85
souza-wallace <walizdesouza781@gmail.com>
2024-03-28 21:30:59 -0300

    resource route

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,13 @@ Route::get('/', function () {
 });
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/clients', [ClientController::class, 'index'])->name('clients.index');
-Route::get('/clients/create', [ClientController::class, 'create'])->name('clients.create');
-Route::post('/clients', [ClientController::class, 'store'])->name('clients.store');
-Route::get('/clients/{client}', [ClientController::class, 'show'])->name('clients.show');
-Route::post('/clients/{client}/edit', [ClientController::class, 'edit'])->name('clients.edit');
-Route::put('/clients/{client}', [ClientController::class, 'update'])->name('clients.update');
-Route::delete('/clients/{client}', [ClientController::class, 'destroy'])->name('clients.destroy');
+// Route::get('/clients', [ClientController::class, 'index'])->name('clients.index');
+// Route::get('/clients/create', [ClientController::class, 'create'])->name('clients.create');
+// Route::post('/clients', [ClientController::class, 'store'])->name('clients.store');
+// Route::get('/clients/{client}', [ClientController::class, 'show'])->name('clients.show');
+// Route::post('/clients/{client}/edit', [ClientController::class, 'edit'])->name('clients.edit');
+// Route::put('/clients/{client}', [ClientController::class, 'update'])->name('clients.update');
+// Route::delete('/clients/{client}', [ClientController::class, 'destroy'])->name('clients.destroy');
+Route::resource('/clients', ClientController::class);
 
 
